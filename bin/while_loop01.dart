301f45121b346ea2a307837c8da5eb8b10832ca3
Пользователь <murodjontohirov2701@gmail.com>
2022-07-12 12:44:06 +0500

    while

--- bin/while_loop01.dart
+++ bin/while_loop01.dart
@@ -2,19 +2,19 @@
   Create function func with "a " argument
   Return the number of odd numbers up to A.
 */
-int getSum(int n) {
-  int s = 0;
-  int d = 0;
-  while (n != 0) {
-    d = n % 10;
-    if (d % 2 == 1) {
-      s += d;
+int getSum(int a) {
+  int b = 0;
+  int count = 0;
+  while (a <= 10) {
+    b++;
+    if (b % 2 == 1) {
+      count++;
     }
-    n ~/= 10;
+    a ~/= 10;
   }
-  return s;
+  return count;
 }
 
 void main() {
-  print(getSum(1234));
+  print(getSum(12));
 }
--- bin/while_loop02.dart
+++ bin/while_loop02.dart
@@ -2,4 +2,18 @@
   Create function func with "a " argument
   Return the number of even numbers up to "a".
 */
-void main() {}
+int func(int a) {
+  int s = 0;
+  int n = 0;
+  while (s <= a) {
+    s++;
+    if (s % 2 == 0) {
+      n += 1;
+    }
+  }
+  return n;
+}
+
+void main() {
+  print(func(10));
+}
