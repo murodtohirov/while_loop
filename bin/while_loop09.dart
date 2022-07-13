d087e903c598ac9015cd7d0ded60b25dc16b3b92
Пользователь <murodjontohirov2701@gmail.com>
2022-07-13 10:48:44 +0500

    2726

--- bin/while_loop09.dart
+++ bin/while_loop09.dart
@@ -6,7 +6,7 @@
 */
 String func(int a) {
   int s = 0;
-  String n = " ";
+  String n = "";
   while (s < a) {
     s += 1;
     if (s % 2 == 0) {
@@ -17,5 +17,5 @@ String func(int a) {
 }
 
 void main() {
-  print(func(7));
+  print(func(6));
 }
