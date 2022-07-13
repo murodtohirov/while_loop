c25af01c5e942908cc3845f53be7812693ee6cb8
Пользователь <murodjontohirov2701@gmail.com>
2022-07-13 10:35:26 +0500

    while10

--- bin/while_loop10.dart
+++ bin/while_loop10.dart
@@ -6,4 +6,25 @@
   
   
 */
-void main() {}
+
+String func(int a) {
+  int i = 0;
+  int count = 0;
+  while (i <= a) {
+    i++;
+    if (a % 2 == 0) {
+      count++;
+    }
+  }
+  String ans = '';
+  if (count == 2) {
+    ans = "tub son";
+  } else {
+    ans = "tub son emas";
+  }
+  return ans;
+}
+
+void main() {
+  print(func(9));
+}
